4e70d6564d32e5dca7d32adb0abd07c52257ea37
Ertiops <104155685+Ertiops@users.noreply.github.com>
2023-08-15 16:48:34 +0300

    changed structure of folders and fixed blueprint static and templates

--- app/blueprints/admin/admin.py
+++ app/blueprints/admin/admin.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template, redirect
 
 
-admin = Blueprint("admin", __name__, template_folder="templates/admin")
+admin = Blueprint("admin", __name__, template_folder="templates", static_folder='static')
 
+# не тргоай пока
 @admin.route("/", methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('sidebar.html')
 
 
 # добавка в бд бронирований
