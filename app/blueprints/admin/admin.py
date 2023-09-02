2a13c971a64d9300e3dfdf598a76c1eff92b8d76
Ertiops <104155685+Ertiops@users.noreply.github.com>
2023-09-02 19:09:28 +0300

    Fixed modals and partially render data

--- app/blueprints/admin/admin.py
+++ app/blueprints/admin/admin.py
@@ -14,20 +14,21 @@ def statistics():
 def franchises():
     franchises = crud.get_franchises_and_franchiser()
     if request.method == 'POST':
-        if request.form['submit'] == "Add":
-            id = uuid.uuid4()
-            city = request.form['city'].capitalize()
-            first_name = request.form['firstName'].capitalize()
-            last_name = request.form['lastName'].capitalize()
-            patronymic = request.form['patronymic'].capitalize()
-            email = request.form['email']
-            phone = request.form['phone']
-            password = request.form['password']
-            role = 'franchiser'
-            franchise_id = id
-            crud.add_franchise(id, city)
-            crud.add_user(first_name, last_name, patronymic, email, phone, password, role, franchise_id)
-            return redirect(url_for('admin.franchises'))
+        print(request.form.to_dict())
+        # if request.form['submit'] == "Add":
+            # id = uuid.uuid4()
+            # city = request.form['city'].capitalize()
+            # first_name = request.form['firstName'].capitalize()
+            # last_name = request.form['lastName'].capitalize()
+            # patronymic = request.form['patronymic'].capitalize()
+            # email = request.form['email']
+            # phone = request.form['phone']
+            # password = request.form['password']
+            # role = 'franchiser'
+            # franchise_id = id
+            # crud.add_franchise(id, city)
+            # crud.add_user(first_name, last_name, patronymic, email, phone, password, role, franchise_id)
+            # return redirect(url_for('admin.franchises'))
         
         # elif request.form['submit'] == "Add":
 
